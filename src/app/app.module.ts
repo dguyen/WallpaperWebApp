a71952dd879b40bc0e535c2e7e26865a5f1fb2d0
David Nguyen <deepfriedcoconut@gmail.com>
2017-12-16 19:13:41 +1100

    Added links to new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
+import { HttpModule } from '@angular/http';
 
+// Components
 import { AppComponent } from './app.component';
 
+// Services
+import { SpotifyService } from './_services/spotify.service';
+import { SpotifyModuleComponent } from './spotify-module/spotify-module.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpotifyModuleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [SpotifyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
