066ff4ab4223d330615ae6d6fbc07d78e97537f8
David Nguyen <deepfriedcoconut@gmail.com>
2017-12-17 01:49:21 +1100

    Added some animations

--- src/app/spotify-module/spotify-module.component.ts
+++ src/app/spotify-module/spotify-module.component.ts
@@ -2,10 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 import { SpotifyService } from '../_services/spotify.service';
 
+import anime from '../../../node_modules/animejs/anime.js';
+import $ from '../../../node_modules/jquery/dist/jquery.js';
+
 @Component({
-  selector: 'app-spotify-module',
-  templateUrl: './spotify-module.component.html',
-  styleUrls: ['./spotify-module.component.css']
+	selector: 'app-spotify-module',
+	templateUrl: './spotify-module.component.html',
+	styleUrls: ['./spotify-module.component.css']
 })
 
 export class SpotifyModuleComponent implements OnInit {
@@ -13,7 +16,6 @@ export class SpotifyModuleComponent implements OnInit {
 	userProfile: any;
 	selectedPlaylist: any;
 	currentView: string = 'loadingView';
-	headerMessage: string = 'Loading';
 
 	constructor(private _spotifyService: SpotifyService){
 		_spotifyService.initialized.then((data) => {
@@ -29,13 +31,10 @@ export class SpotifyModuleComponent implements OnInit {
 	initialize(){
 		this._spotifyService.getUserProfile().subscribe(
 			res => {
-				console.log(res);
 				this.userProfile = res;
-				this.headerMessage = 'Welcome ' + res.id;
+				this.changeHeaderText('Welcome ' + res.id);
 			},
-			err => {
-				console.log(err);
-			});	
+			err => console.log(err));	
 
 		this._spotifyService.getUserPlaylists().subscribe(
 			res => {
@@ -43,19 +42,25 @@ export class SpotifyModuleComponent implements OnInit {
 				this.playlists = res.items;
 				this.changeView('playlistView');
 			},
-			err => {
-				console.log(err);
-			});
+			err => console.log(err));
 	}
 
 	changeView(view){
-		console.log('Attempting to changing view...');
 		if(view != 'loadingView' && view != 'playlistView' && view != 'songView')
 			console.log('Invalid view');
 		
+		if(this.currentView == 'loadingView'){
+			anime({
+				targets: '#bodyContainer',
+				height: [0, 450],
+				easing: 'easeOutExpo',
+				duration: 1000
+			})
+		}
 		this.currentView = view;
 		console.log('View changed to ' + view);
 	}
+
 
 	selectPlaylist(playlistSelected){
 		console.log('Playlist: ' + playlistSelected.name);
@@ -107,5 +112,57 @@ export class SpotifyModuleComponent implements OnInit {
 		this._spotifyService.mediaSeek(method);
 	}
 
+
+
+	changeHeaderText(newText: string){			
+		$('#headerLetters').each(function(){
+			$(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span id='headerLetters'>$&</span>"));
+		});
+		anime({
+			targets: '#headerLetters',
+			scaleX: [1, 0],
+			opacity: [1, 0],
+			duration: 500,
+			easing: 'easeOutExpo'
+		}).finished.then(() => {
+			$('#headerLetters').text(newText)
+			$('#headerLetters').each(function(){
+				$(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span id='headerLetters'>$&</span>"));
+			});
+		
+			anime.timeline()
+			.add({
+				targets: '#line',
+				scaleY: [0, 1],
+				opacity: [0.5, 1],
+				easing: "easeOutExpo",
+				duration: 700
+			})
+			.add({
+				targets: '#line',
+				translateX: [0, $("#headerLetters").width()],
+				easing: "easeOutExpo",
+				duration: 700,
+				delay: 100
+			}).add({
+				targets: '#headerLetters',
+				opacity: [0, 1],
+				easing: "easeOutExpo",
+				duration: 600,
+				offset: '-=775',
+				delay: function(el, i) {
+					return 34 * (i+1)
+				}
+			}).add({
+				targets: '#line',
+				translateX: [$("#headerLetters").width()],
+				scaleY: [1, 0],
+				opacity: [1, 0],
+				easing: "easeOutExpo",
+				duration: 700
+			})
+
+		})
+	}
 
 }
