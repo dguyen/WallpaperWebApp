3c458aa2cf5a7f45cfef29dea1efa402bec62baa
David Nguyen <deepfriedcoconut@gmail.com>
2017-12-21 02:06:27 +1100

    Fixed compile error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,28 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { HttpModule } from '@angular/http';
 
-// Components
-import { AppComponent } from './app.component';
-
 // Services
 import { SpotifyService } from './_services/spotify.service';
+import { WeatherService } from './_services/weather.service';
+
+
+// Components
+import { AppComponent } from './app.component';
 import { SpotifyModuleComponent } from './spotify-module/spotify-module.component';
+import { WeatherModuleComponent } from './weather-module/weather-module.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SpotifyModuleComponent
+    SpotifyModuleComponent,
+    WeatherModuleComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     FormsModule
   ],
-  providers: [SpotifyService],
+  providers: [SpotifyService, WeatherService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
